06fd801cc0e054e2a3f040c79a397842deae304a
Sameem p <103162659+QUEEN-NISHIMIYA@users.noreply.github.com>
2024-11-25 19:48:39 +0530

    💗ɪᴢᴜᴍɪᴇ-ᴋᴜɴ💗

--- server.js
+++ server.js
@@ -2,28 +2,20 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
-
 dotenv.config();
 connectDB();
-
 const app = express();
-app.use(express.json()); // For parsing JSON
+app.use(express.json());
 app.use(cors({
-  origin: ["https://izumie.rf.gd"], // Add your InfinityFree subdomain here
+  origin: ["https://izumie.rf.gd"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
-
-// Import Routes
 const authRoutes = require("./routes/auth");
 const kycRoutes = require("./routes/kyc");
 const userRoutes = require("./routes/user");
-
-// API Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/kyc", kycRoutes);
 app.use("/api/user", userRoutes);
-
-// Server Listener
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
