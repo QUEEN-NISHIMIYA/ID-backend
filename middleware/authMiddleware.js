ead94e91d03f80bbf76d7d350732093833c2448d
Sameem p <103162659+QUEEN-NISHIMIYA@users.noreply.github.com>
2024-11-25 19:21:56 +0530

    💗ɪᴢᴜᴍɪᴇ-ᴋᴜɴ💗

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,27 +1,19 @@
 const jwt = require("jsonwebtoken");
-
-const adminEmails = ["braveizumie@outlook.com"]; // Add your admin emails here
-
+const adminEmails = ["braveizumie@outlook.com"];
 const authenticateUser = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
-
   if (!token) {
     return res.status(401).json({ message: "Unauthorized" });
   }
-
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
-
-    // Check if the user is an admin
     if (adminEmails.includes(decoded.email)) {
-      req.isAdmin = true; // Add admin flag
+      req.isAdmin = true;
     }
-
     next();
   } catch (err) {
     return res.status(401).json({ message: "Invalid token" });
   }
 };
-
 module.exports = authenticateUser;
