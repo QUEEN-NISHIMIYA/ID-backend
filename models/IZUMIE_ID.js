015c6a04f2157abe7c4082bbf16c93dc41590a3d
Sameem p <103162659+QUEEN-NISHIMIYA@users.noreply.github.com>
2024-11-26 12:03:04 +0530

    💗ɪᴢᴜᴍɪᴇ-ᴋᴜɴ💗

--- models/IZUMIE_ID.js
+++ models/IZUMIE_ID.js
@@ -1,1 +1,10 @@
 
+const mongoose = require("mongoose");
+
+const izumieIDSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  izumieID: { type: String, unique: true, required: true },
+  issuedAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("IZUMIE_ID", izumieIDSchema);
