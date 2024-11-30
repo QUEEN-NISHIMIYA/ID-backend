09e39a5cc44f065a0e424cd832d5e287d922a8b4
Sameem p <103162659+QUEEN-NISHIMIYA@users.noreply.github.com>
2024-11-30 20:30:37 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,10 +2,25 @@ const { DataTypes } = require("sequelize");
 const { sequelize } = require("../config/db");
 
 const User = sequelize.define("User", {
-  email: { type: DataTypes.STRING, allowNull: false, unique: true },
-  password: { type: DataTypes.STRING, allowNull: false },
-  izumieID: { type: DataTypes.STRING, unique: true },
-  kycStatus: { type: DataTypes.STRING, defaultValue: "pending" },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  izumieID: {
+    type: DataTypes.STRING,
+    unique: true,
+  },
+  twoFactorSecret: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+}, {
+  timestamps: true,
 });
 
 module.exports = User;
